19938129482982f9a85aa2bb47310c5707057bb5
DanielGallardo00 <59607988+DanielGallardo00@users.noreply.github.com>
2023-10-09 13:36:49 +0200

    Logica general del juego

--- app/src/main/java/com/example/myapplication/JuegoActivity.java
+++ app/src/main/java/com/example/myapplication/JuegoActivity.java
@@ -2,23 +2,121 @@ package com.example.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.graphics.Color;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class JuegoActivity extends AppCompatActivity {
+public class JuegoActivity extends AppCompatActivity implements View.OnClickListener{
 
-    private int puntos;
+
+    public static String question[] ={
+            "¿De qué color fue el primer Hulk?",
+            "¿Cada cuántos minutos tenían que pulsar la tecla en LOST?",
+            "¿Cómo se llama la digievolución de Gabumon?",
+            "'Nada es verdad...' ",
+            "¿Quién de las siguientes fotografias es Power de Chainsaw Man Chainsaw?"
+    };
+
+    public static String choices[] []={
+            {"Gris","Verde","Rojo","Blanco"},
+            {"42","120","108","55"},
+            {"Greymon","Súper Gabumon","Angemon","Garurumon"},
+            {"salvo algunas cosas'","hasta que se hace realidad'","aunque en realidad si'","todo esta permitido'"}
+    };
+
+    public static String correct[] ={
+            "Gris",
+            "108",
+            "Garurumon",
+            "todo esta permitido'"
+    };
+    private int score=0;
+    TextView questionTextView;
+    Button ansA, ansB, ansC, ansD;
+    Button submitBtn;
+
+    int totalQuestion = question.length;
+    int currentQuestionIndex = 0;
+    String selectedAnswer = "";
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_juego);
+
+        questionTextView = findViewById(R.id.question);
+        ansA = findViewById(R.id.ans_A);
+        ansB = findViewById(R.id.ans_B);
+        ansC = findViewById(R.id.ans_C);
+        ansD = findViewById(R.id.ans_D);
+        submitBtn = findViewById(R.id.submit_btn);
+
+        ansA.setOnClickListener(this);
+        ansB.setOnClickListener(this);
+        ansC.setOnClickListener(this);
+        ansD.setOnClickListener(this);
+        submitBtn.setOnClickListener(this);
+
+        loadNewQuestion();
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        ansA.setBackgroundColor(Color.WHITE);
+        ansB.setBackgroundColor(Color.WHITE);
+        ansC.setBackgroundColor(Color.WHITE);
+        ansD.setBackgroundColor(Color.WHITE);
+
+        Button clickedButton = (Button) view;
+        if(clickedButton.getId()==R.id.submit_btn){
+            if(selectedAnswer.equals(correct[currentQuestionIndex])){
+                score++;
+            }
+            currentQuestionIndex++;
+            loadNewQuestion();
+
+
+        }else{
+            //choices button clicked
+            selectedAnswer  = clickedButton.getText().toString();
+            clickedButton.setBackgroundColor(Color.MAGENTA);
+
+        }
+
+    }
+
+    void loadNewQuestion(){
+
+        if(currentQuestionIndex == totalQuestion ){
+            finishQuiz();
+            return;
+        }
+
+        questionTextView.setText(question[currentQuestionIndex]);
+        ansA.setText(choices[currentQuestionIndex][0]);
+        ansB.setText(choices[currentQuestionIndex][1]);
+        ansC.setText(choices[currentQuestionIndex][2]);
+        ansD.setText(choices[currentQuestionIndex][3]);
+
+    }
+
+    void finishQuiz(){
+        String passStatus = "";
+        if(score > totalQuestion*0.60){
+            passStatus = "Passed";
+        }else{
+            passStatus = "Failed";
+        }
     }
 
 
     private void endGame(){
 
         Intent intent = new Intent(getApplicationContext(), JuegoActivity.class);
-        intent.putExtra("puntos",puntos);   //para pasar los puntos de un activity a otro
+        intent.putExtra("puntos",score);   //para pasar los puntos de un activity a otro
 
         startActivity(intent);
     }
